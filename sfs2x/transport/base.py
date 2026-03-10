b9e3554aedbe8896cf7d5e7df023ca8e25b43fc0
mlnitoon2 <77251330+mlnitoon2@users.noreply.github.com>
2026-03-10 13:04:54 -0500

    Update base.py

--- sfs2x/transport/base.py
+++ sfs2x/transport/base.py
@@ -1,10 +1,10 @@
 from abc import ABC, abstractmethod
 from collections.abc import AsyncIterator
 from typing import Protocol
+import hashlib
 
-from sfs2x.core import Buffer
-from sfs2x.protocol import Message, decode, encode
-
+from sfs2x.core import Buffer, SFSObject, SFSArray
+from sfs2x.protocol import Message, decode, encode, Room, SysAction, ControllerID
 
 class Transport(ABC):
     """Abstract base class for transports."""
@@ -41,12 +41,54 @@ class Transport(ABC):
             self._closed = True
 
     async def listen(self) -> AsyncIterator[Message]:
-        """Async iterator over incoming messages."""
         while not self._closed:
             try:
-                yield await self.recv()
+                msg = await self.recv()
+                handled = await self._handle_system(msg)
+                if not handled:
+                    yield msg
+                yield msg
             except (ConnectionError, RuntimeError):
                 break
+    async def _handle_system(self, message: Message) -> bool:
+        payload = message.payload
+        if message.action == SysAction.HANDSHAKE:
+            token = hashlib.md5(self.host().encode()).hexdigest()
+    
+            session_info = SFSObject()
+            session_info.put_int("ct", 1000000)
+            session_info.put_int("ms", 8000000)
+            session_info.put_utf_string("tk", token)
+    
+            await self.send(Message(
+                controller=ControllerID.SYSTEM,
+                action=SysAction.HANDSHAKE,
+                payload=session_info
+            ))
+    
+            return True
+        elif message.action == SysAction.LOGIN:
+            login = SFSObject()
+            login.put_short("rs", 0)
+            login.put_utf_string("zn", self.zone_name)
+            login.put_utf_string("un", payload.get("un"))
+            login.put_short("pi", 0)
+
+            NewRoom = Room(room_id=0, name="Limbo", room_type="default", is_hidden=False, 
+                    is_password_protected=True, is_game=False, min_players=0, max_players=self.max_players)
+
+            RoomArrays = SFSArray()
+            RoomArrays.add_sfs_array(NewRoom.to_sfs_array())
+
+            login.put_sfs_array("rl", RoomArrays)
+
+            await self.send(Message(
+                controller=ControllerID.SYSTEM,
+                action=SysAction.LOGIN,
+                payload=login
+            ))
+    
+        return False
 
     async def __aenter__(self) -> "Transport":
         """Async enter."""
